f95535638ae145935459b1f32aece50f7aec018a
samp1etex7 <samp1etex7@gmail.com>
2018-04-18 10:47:39 +0300

    Fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418072308) do
+ActiveRecord::Schema.define(version: 20180418073956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20180418072308) do
     t.boolean "approved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating", default: 0
   end
 
   create_table "articles_categories", force: :cascade do |t|
