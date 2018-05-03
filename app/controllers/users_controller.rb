d3d12f9cf0ac407ca184368dc6b6e5b6cd47d4c0
nikolai510 <meisterofcode@gmail.com>
2018-05-03 14:16:42 +0300

    fix_search_users_and_articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,30 @@
 class UsersController < ApplicationController
 
-  # def index
-  #   @users = User.all
-  #   if params[:follows]
-  #     @users = @users.where(id: current_user.subscriptions_ids)
-  #   end
-  #   @users = @users.where("email ILIKE :str OR name ILIKE :str", str: "%#{params[:search]}%") if params[:search]
-  #   @users = @users.order('created_at DESC')
-  #   @users = @users.page_kaminari(params[:page])
-  # end
+	# def index
+	#   @users = User.all
+	#   if params[:follows]
+	#     @users = @users.where(id: current_user.subscriptions_ids)
+	#   end
+	#   @users = @users.where("email ILIKE :str OR name ILIKE :str", str: "%#{params[:search]}%") if params[:search]
+	#   @users = @users.order('created_at DESC')
+	#   @users = @users.page_kaminari(params[:page])
+	# end
 
-  def index
-    page = params[:page] || 1
-    @users_search = User.ransack(params[:q])
-    @users = @users_search.result
-  end
+	def index
+		page = params[:page] || 1
+		if params[:q].blank?
+			@users = []
+			@articles = []
+		else
+			@users = User.search(name_or_lastname_cont: params[:q]).result
+			@articles = Article.search(header_or_body_cont: params[:q]).result
+		end
+	end
 
-  def subscribe
-    @user = User.find(params[:id])
-    current_user.sub_unsub_user(@user)
-    redirect_to users_path
-  end
+	def subscribe
+		@user = User.find(params[:id])
+		current_user.sub_unsub_user(@user)
+		redirect_to users_path
+	end
 
 end
