4d4783e690854dc16fcce370453b56e21a0f0572
samp1etex7 <samp1etex7@gmail.com>
2018-05-10 11:39:14 +0300

    Add excercises to admin

---
+++ app/admin/exercise.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register Exercise do
+  menu priority: 6
+  config.batch_actions = true
+  actions :all, :except => [:show, :destroy]
+  menu label: "Упражнения"
+
+  filter :exercise_name
+
+  permit_params :exercise_name, :exercise_description
+
+  index do
+    selectable_column
+    id_column
+    column :exercise_name
+    column :exercise_description
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :exercise_name
+      f.input :exercise_description
+    end
+    f.actions
+  end
+
+
+end
--- app/admin/trainer.rb
+++ app/admin/trainer.rb
@@ -17,7 +17,6 @@ ActiveAdmin.register Trainer do
     actions
   end
 
-
   form do |f|
     f.inputs do
       f.input :fio
